55d6d12e3d2da1e2bb1be64e41dacf1431817864
Guilherme-Luzin <guilherme.carvalho@inventsoftware.com.br>
2022-10-27 15:49:23 -0300

    Adicionando modal no cadastro de categoria e adicionando useEffect para o botao de voltar

--- src/Pages/Begin/Index.js
+++ src/Pages/Begin/Index.js
@@ -2,9 +2,8 @@ import {
   View,
   Text,
   TouchableOpacity,
-  StyleSheet, 
+  StyleSheet,
 } from 'react-native'
-import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { AntDesign } from '@expo/vector-icons'
 
